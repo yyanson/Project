42f5071485a3cd9894bf89a744528db89e3bc1af
yyanson <91633606+yyanson@users.noreply.github.com>
2021-10-09 17:30:48 +0300

    Add 6 tasks
    Adding 6 tasks in lesson 1 on Python

---
+++ step_1.py
@@ -0,0 +1,36 @@
+import tkinter
+
+#print('Hello world!')
+
+sht = len(input('Укажите численность сотрудников фирмы: '))
+print(type(sht), sht)
+'''
+from keyword import kwlist
+print(kwlist)
+oct1, oct2, oct3, oct4 = [10, 1, 1, 1]
+print(f'IP address: {oct1:<8} {oct2:<8} {oct3:<8} {oct4:<8}')
+print(f'IP address: {oct2:<8} {oct1:<8} {oct3:<8} {oct4:<8}')
+print(f'IP address: {oct1:<8} {oct3:<8} {oct2:<8} {oct4:<8}')
+
+octets = ['10', '1', '1', '1']
+mask = 10
+print(f"ip-params: {'/'.join(octets)}, mask: {mask}")
+ip = '192.168.1.4'
+mask = 10
+print(f"ip-params: {ip}, mask: {mask}")
+
+print("{:.3f}".format(5/0.3))
+print('{}'.format(['el_1', 'el_2', 'el_3', 'el_4']))
+print('Третий элемент: {2}; Второй элемент: {1}; Первый элемент: {0}'.format('el_1', 'el_2', 'el_3'))
+print(['el_1', 'el_2', 'el_3', 'el_4'])
+print("{:>20} {:>20} {:>20}".format('my_param_1', 'my_param_2', 'my_param_3'))
+print("{:<20} {:>20} {:>20}".format('my_param_1', 'my_param_2', 'my_param_3'))
+
+print(20.0/8)
+print("%.2f" % (20.0/8))
+print("%.2d" % (20/8))
+
+#print("%-10s %-10s %-10s" %.('papam1', 'papam2', 'papam3'))
+print("%-20s %-10s %-5s" % ('papam1', 'papam2', 'papam3'))
+print("%-20s %-10s %s" % ('papam1', 'papam2', 'papam3'))
+'''
